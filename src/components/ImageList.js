49ac5665566d8039ac7fb04017f7da588fc88042
shawn <= linxiang08@gmail.com>
2019-04-03 03:06:09 -0400

    second

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import './ImageList.css';
+import ImageCard from './ImageCard'
 
 const ImageList = (props) => {
-    const images = props.images.map(img => <img src={img.urls.regular}/>);
-    return <div>{images}</div>;
+    const images = props.images.map(image => {
+        return <ImageCard key={image.id} image={image}/>
+    });
+
+    return <div className='image-list'>{images}</div>
+
 
 };
 
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
 
 class SearchBar extends Component {
-    
-    state = {term: 'Namask',};
+
+    state = {term: 'car',};
 
     onFormSubmit = (event) =>{
         event.preventDefault();
